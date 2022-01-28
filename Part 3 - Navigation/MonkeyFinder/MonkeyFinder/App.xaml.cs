1f9c544ac7679d4db33c0732af5ac36246e9ef1b
James Montemagno <james.montemagno@gmail.com>
2022-01-28 11:31:27 -0800

    Update to latest nugets, light theme only,

--- Part 3 - Navigation/MonkeyFinder/MonkeyFinder/App.xaml.cs
+++ Part 3 - Navigation/MonkeyFinder/MonkeyFinder/App.xaml.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.AppCenter.Analytics;
-using Microsoft.AppCenter.Crashes;
-using Microsoft.AppCenter;
-using MonkeyFinder.View;
+﻿using MonkeyFinder.View;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Device = Xamarin.Forms.Device;
@@ -25,19 +22,6 @@ namespace MonkeyFinder
                 BarBackgroundColor = (Color)Resources["Primary"],
                 BarTextColor = Color.White
             };
-        }
-
-        const string AppCenteriOS = "";
-        const string AppCenterAndroid = "";
-
-        protected override void OnStart()
-        {
-            if (!string.IsNullOrWhiteSpace(AppCenteriOS) && !string.IsNullOrWhiteSpace(AppCenterAndroid))
-            {
-                AppCenter.Start($"ios={AppCenteriOS};" +
-                      $"android={AppCenterAndroid}",
-                      typeof(Analytics), typeof(Crashes));
-            }
         }
 
         protected override void OnSleep()
--- Part 4 - Native Features/MonkeyFinder/MonkeyFinder.Android/MainActivity.cs
+++ Part 4 - Native Features/MonkeyFinder/MonkeyFinder.Android/MainActivity.cs
@@ -6,7 +6,7 @@ using Android.OS;
 
 namespace MonkeyFinder.Droid
 {
-    [Activity(Label = "MonkeyFinder", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "MonkeyFinder", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
